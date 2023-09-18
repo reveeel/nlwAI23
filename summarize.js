aad525420c25aa7de3e11a79329fb8f6e17b1138
Miguel Paiva de Oliveira <125935124+reveeel@users.noreply.github.com>
2023-09-17 21:30:05 -0300

    Add files via upload

---
+++ summarize.js
@@ -0,0 +1,23 @@
+import { Pipeline, pipeline } from "@xenova/transformers"
+import { summaryExample } from "./utils/summary.js"
+
+export async function summarize(text) {
+  try {
+    //  return summaryExample
+
+    console.log("Gerando o resumo...")
+
+    const generator = await pipeline(
+      "summarization",
+      "Xenova/distilbart-cnn-12-6"
+    )
+
+    const output = await generator(text)
+
+    console.log("Resumo concluído com sucesso!")
+    return output[0].summary_text
+  } catch (error) {
+    console.log("Não foi possivel realizar o resumo", error)
+    throw new Error(error)
+  }
+}
